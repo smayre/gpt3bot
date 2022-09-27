a5eacea4421f193aff40bb14a46814a641f7974d
Simon Ayre <smayre@users.noreply.github.com>
2022-09-27 11:23:13 +1000

    Move generate_reply func

--- gpt3bot.py
+++ gpt3bot.py
@@ -2,9 +2,12 @@
 
 import os
 from slack_bolt import App
-from gpt3wrapper import generate_reply
+from gpt3wrapper import get_gpt3_completion
+from datetime import datetime as dt
+
 
 BOT_USERID = os.environ["SLACK_BOT_ID"]
+STOP_TOKEN = "<EOT>"
 
 
 app = App(
@@ -25,6 +28,30 @@ def reply_to_mention(logger, client, event, say):
     except Exception as e:
         logger.error(e)
         raise
+
+
+def generate_reply(message_history, bot_userid, stop_token=STOP_TOKEN):
+    """Create a prompt for GPT-3 by converting a Slack conversation
+    history into a chat log. Append the STOP_TOKEN to the end of each
+    message so that (hopefully) GPT-3 will do the same, which we then
+    use to cut off GPT-3's completion. Otherwise GPT-3 may produce a fake
+    conversation with itself."""
+
+    messages = [
+        (dt.fromtimestamp(float(msg["ts"])), msg["user"], msg["text"])
+        for msg in message_history
+        if msg["text"].strip() != f"<@{bot_userid}>"
+    ]
+    messages.sort(key=lambda m: m[0])
+    chatlog = [
+        f"{ts.isoformat()} {userid}: {text}{stop_token}"
+        for ts, userid, text in messages
+    ]
+    now = dt.now().isoformat()
+    chatlog.append(f"{now} {bot_userid}:")
+    prompt = "\n".join(chatlog)
+    gpt3_reply = get_gpt3_completion(prompt, stop_token)
+    return gpt3_reply
 
 
 if __name__ == "__main__":
