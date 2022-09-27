edf8942f0c6a38ed2359af45d9fd1db08ee451c7
Simon Ayre <smayre@users.noreply.github.com>
2022-09-27 11:17:16 +1000

    Remove app home event

--- gpt3bot.py
+++ gpt3bot.py
@@ -3,7 +3,6 @@
 import os
 from slack_bolt import App
 from gpt3wrapper import generate_reply
-import json
 
 BOT_USERID = os.environ["SLACK_BOT_ID"]
 
@@ -12,43 +11,6 @@ app = App(
     token=os.environ["SLACK_BOT_TOKEN"],
     signing_secret=os.environ["SLACK_SIGNING_SECRET"],
 )
-
-
-def block(type, **kwargs):
-    return {"type": type, **kwargs}
-
-
-def section(*args, **kwargs):
-    return block(text=block(*args, **kwargs), type="section")
-
-
-@app.event("app_home_opened")
-def update_home_tab(client, event, logger):
-    try:
-        blocks = [
-            section("mrkdwn", text="*GPT-3 bot*"),
-            block(type="divider"),
-            section("mrkdwn", text="WIP"),
-            block(
-                type="actions",
-                elements=[
-                    block(
-                        type="button", text=block(type="plain_text", text="Do nothing")
-                    )
-                ],
-            ),
-        ]
-        client.views_publish(
-            user_id=event["user"],
-            view=dict(
-                type="home",
-                callback_id="home_view",
-                blocks=blocks,
-            ),
-        )
-    except Exception as e:
-        logger.error(f"Error publishing home tab: {e}")
-        logger.error(json.dumps(blocks, indent=4))
 
 
 @app.event("app_mention")
