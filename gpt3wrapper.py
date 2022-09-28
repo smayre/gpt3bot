8384c62767b8ca96976d6edc1586162a4014cb84
Simon Ayre <smayre@users.noreply.github.com>
2022-09-28 10:53:24 +1000

    Set frequency penalty on GPT-3 completions

--- gpt3wrapper.py
+++ gpt3wrapper.py
@@ -6,22 +6,21 @@ import random
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-MODEL = "text-davinci-002"
-TEMPERATURE = 0.7
+GPT_PARAMS = dict(
+    model="text-davinci-002",
+    temperature=0.7,
+    max_tokens=128,
+    top_p=1,
+    frequency_penalty=1,
+    presence_penalty=0,
+)
 
 
-def get_gpt3_completion(
-    prompt, stop_token, model=MODEL, temperature=TEMPERATURE
-):
+def get_gpt3_completion(prompt, stop_token):
     response = openai.Completion.create(
-        model=model,
         prompt=prompt,
-        temperature=temperature,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
         stop=[stop_token],
+        **GPT_PARAMS,
     )
     choice = random.choice(response["choices"])
     return choice["text"]
