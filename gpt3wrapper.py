0b3cf8e865f1ff0fc6684e2c82c6dcf90277be63
Simon Ayre <smayre@users.noreply.github.com>
2022-10-03 13:40:05 +1100

    Add usage logging and thresholds

--- gpt3wrapper.py
+++ gpt3wrapper.py
@@ -2,11 +2,20 @@
 
 import os
 import openai
+from datetime import datetime as dt
+from datetime import timedelta
+from usagelog import log_usage, get_cost
+import sys
+
+COST_LIMIT = 0.10  # USD
+LIMIT_PERIOD = 4  # hours
+
+RICH_MODEL = "text-davinci-002"
+POOR_MODEL = "text-babbage-001"
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 GPT_PARAMS = dict(
-    model="text-davinci-002",
     temperature=0.7,
     max_tokens=128,
     top_p=1,
@@ -16,13 +25,25 @@ GPT_PARAMS = dict(
 
 
 def get_gpt3_completion(prompt, stop_token, choices=1):
+    period_start = dt.now() - timedelta(hours=LIMIT_PERIOD)
+    cost = get_cost(start=period_start.timestamp())
+    model = RICH_MODEL if cost < COST_LIMIT else POOR_MODEL
     response = openai.Completion.create(
+        model=model,
         prompt=prompt,
         stop=[stop_token],
         n=choices,
         best_of=choices,
         **GPT_PARAMS,
     )
+    try:
+        log_usage(
+            response["created"],
+            response["usage"]["total_tokens"],
+            response["model"],
+        )
+    except BaseException:
+        sys.exit(1)
     return [choice["text"] for choice in response["choices"]]
 
 
---
+++ usagelog.py
@@ -0,0 +1,79 @@
+import os
+import sqlite3
+from datetime import datetime as dt
+from datetime import timedelta
+import sys
+
+
+USAGE_LOG = os.environ.get("USAGE_LOG", "/tmp/usage.db")
+
+# cost per 1k tokens, USD
+PRICING = {
+    "text-ada-001": 0.0004,
+    "text-babbage-001": 0.0005,
+    "text-curie-001": 0.002,
+    "text-davinci-002": 0.02,
+}
+
+
+def set_up_usage_log(path):
+    conn = sqlite3.connect(path)
+    conn.execute("create table pricing (model text primary key, rate real)")
+    conn.execute(
+        "create table usage ("
+        "    ts real primary key,"
+        "    tokens integer,"
+        "    model text references pricing (model))"
+    )
+    conn.execute(
+        "create view costs as "
+        "   select u.*, p.rate * (u.tokens/1000.0) as cost"
+        "   from usage u left join pricing p"
+        "   on u.model = p.model"
+    )
+    for m, p in PRICING.items():
+        conn.execute("insert into pricing values (?, ?)", [m, p])
+    conn.commit()
+    conn.close()
+
+
+def log_usage(ts, tokens, model, logfile=USAGE_LOG):
+    conn = sqlite3.connect(logfile)
+    conn.execute("PRAGMA foreign_keys = 1")
+    conn.execute("insert into usage values (?, ?, ?)", [ts, tokens, model])
+    conn.commit()
+    conn.close()
+
+
+def get_cost(start=0, logfile=USAGE_LOG):
+    conn = sqlite3.connect(logfile)
+    ((res,),) = conn.execute(
+        "select sum(cost) from costs where ts > ?", [start]
+    )
+    return res or 0
+
+
+if not os.path.exists(USAGE_LOG):
+    try:
+        set_up_usage_log(USAGE_LOG)
+    except BaseException:
+        os.remove(USAGE_LOG)
+        raise
+else:
+    assert os.path.isfile(USAGE_LOG)
+
+
+if __name__ == "__main__":
+    try:
+        hours = sys.argv[1]
+    except IndexError:
+        hours = 24
+    start = (dt.now() - timedelta(hours=hours)).timestamp()
+    conn = sqlite3.connect(USAGE_LOG)
+    cursor = conn.execute(
+        "select * from costs where ts > ? order by 1", [start]
+    )
+    for ts, tokens, model, cost in cursor:
+        ts_str = dt.fromtimestamp(ts).strftime("%d-%b-%Y %H:%M:%S").upper()
+        print(f"{ts_str}: ${cost} ({tokens}/{model})")
+    print(f"${get_cost(start)}")
